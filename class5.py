2d893446df75fafc4ab080306139ca08e16a746c
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2025-12-22 20:27:54 +0530

    add new class

---
+++ class5.py
@@ -0,0 +1,37 @@
+# comparision operators 
+
+# == equal to
+# != not equal to
+# > greater than
+# < less than
+# >= greater than or equal to
+# <= less than or equal to
+
+a = 10
+b = 20
+
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+
+
+# and or not operators
+
+# and = True and True = True
+
+a = 10
+b = 20
+print(a > b and a != b)
+
+
+# or = True or False = True
+
+a = 10
+b = 20
+print(a > b or a != b)
+
+# not = not True = False
+print(10>=10)
