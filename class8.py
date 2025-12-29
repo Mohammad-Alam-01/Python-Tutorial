12ad790cdedc449fb18a2d3c14dffc3870ee0e66
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2025-12-29 22:20:23 +0530

    add new

--- class8.py
+++ class8.py
@@ -1,1 +1,13 @@
 # class 8
+
+# import random
+# import string
+
+# def generate_password(length=12):
+#     characters = string.ascii_letters + string.digits + string.punctuation
+#     password = ''.join(random.choice(characters) for _ in range(length))
+#     return password
+
+# # User se password length lena
+# length = int(input("Enter password length: "))
+# print("Generated Password:", generate_password(length))
