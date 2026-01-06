83dee63d756a824b3a58bb66a8fb617c642f7cc8
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2026-01-06 21:02:23 +0530

    add new data

--- class8game.py
+++ class8game.py
@@ -22,16 +22,41 @@ import random
 # user = input("Enter your choice (stone, paper, scissors): ")
 # computer = random.choice(["stone", "paper", "scissors"])
 
-user = int(input("Enter your choice (1 for stone, 2 for paper, 3 for scissors): "))
-computer = random.randint(1,3)
 
-if user == 1 and computer == 3:
-    print("You win! Stone crushes scissors.")
-elif user == 2 and computer == 1:
-    print("You win! Paper covers stone.")
-elif user == 3 and computer == 2:
-    print("You win! Scissors cut paper.")
-elif user == computer:
-    print("It's a tie!")
-else:
-    print("Computer wins!")
+cscore = 0
+hscore = 0 
+
+while True:
+    print(f"current scores you - {hscore} computer - {cscore}\n")
+    user = int(input("1 for stone , 2 for paper , 3 for scissors choose :- "))
+
+    com = random.randint(1,3)
+
+    if user == 1 and com == 3:
+        hscore+=1 
+        print("you won the round \n")
+
+    elif user == 2 and com == 1:
+        hscore+=1 
+
+        print("you won the round \n")
+
+    elif user == 3 and com == 2:
+        hscore+=1 
+
+        print("you won the round \n")
+
+    elif user == com:
+        print("it was a draw")
+
+    else:
+        cscore+=1 
+        print("computer won this round ")
+    
+
+    if cscore == 5:
+        print("conmputer won this game 👿")
+        break
+    elif hscore == 5:
+        print("congratulations you won 🏅")
+        break
