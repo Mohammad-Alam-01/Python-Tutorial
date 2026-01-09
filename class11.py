76d500aa4966f67ff3dc30b8c0402743ba94bfa5
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2026-01-09 20:47:16 +0530

    add new class

---
+++ class11.py
@@ -0,0 +1,11 @@
+#  List 
+# my_list = [10, 20, 30, 40, 50]
+
+# We can store anithing in list
+# my_list = [10, "Hello", 45.67, True, None]
+
+# we can have duplicate values in list
+# my_list = [10, 20, 30, 20, 10, 40]
+
+# We can access list items by index
+# print(my_list[0])  # 10
