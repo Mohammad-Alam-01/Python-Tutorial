c926a5eb8ddeaa95793a6bba3c0209d769b7d9ed
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2026-01-17 21:48:47 +0530

    add  new data

--- class12.py
+++ class12.py
@@ -19,12 +19,36 @@
 
 
 # # tuple unpaking
-tuple = (10,20,30)
-a, b, c = tuple # tuple unpaking
-print(tuple)
-print(a, b, c)
+# tuple = (10,20,30)
+# a, b, c = tuple # tuple unpaking
+# print(tuple)
+# print(a, b, c)
 
 
 
 
+# # Tuple traversing
 
+# tuple = (10,20,30,40,50)
+# # 1st way
+# for i in tuple:
+#     print(i)
+
+# # 2nd way
+# for i in range(len(tuple)):
+#     # print(tuple[i])
+#     print(i, ":", tuple[i])
+
+
+
+
+# # Tuple methods
+# # tuple has only two methods count() and index()
+
+# 1st method count()
+
+tuple = (10,20,10,10,20,20,30,40,50,10)
+print(tuple.count(10))  # output 4
+
+tuple = (10,20,30,40,50)
+print(tuple.index(30)) #Output 2
