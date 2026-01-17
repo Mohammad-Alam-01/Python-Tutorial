ea3cf6d875a164329293d7706a088935f6573cc4
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2026-01-17 21:24:09 +0530

    add new data

--- class12.py
+++ class12.py
@@ -1,11 +1,22 @@
 # # tuple has hytrogenius nature
 
-tuple = (10,20,30,40,50,60,70,80,90,100)
-print(type(tuple))
-print(tuple)
+# tuple = (10,20,30,40,50,60,70,80,90,100)
+# print(type(tuple))
+# print(tuple)
 
 
 # # tuple has immutable nature
+
+# tuple = (10,20,10,10,20,20,30,40,50)
+# print(tuple)
+
+
+# # we can't change value of tuple
+
+tuple = (10,20,30,40,50)
+# tuple[2] = 100  # TypeError: 'tuple' object does not support item assignment
+print(tuple)
+
 # # we can store same value mutiple time in tuple
 
 
