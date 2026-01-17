7b09a48fddc3d75326d325f69e519ab230a0e39e
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2026-01-17 20:45:18 +0530

    add new data

--- class11_list.py
+++ class11_list.py
@@ -208,16 +208,4 @@
 #     my_list[i], my_list[min_index] = my_list[min_index], my_list[i]
 
 # print(my_list)
-
-
-
-my_list = [89, 45, 23, 67, 34, 12, 10, 5, 78, 56, 90]
-
-for i in range(len(my_list)-1):
-    min_index = i
-    for j in range(i+1, len(my_list)):
-        if my_list[j] < my_list[min_index]:
-            min_index = j
-    
-    my_list[i], my_list[min_index] = my_list[min_index], my_list[i]
-print(my_list)
+# # Question 12: WAP to Selection sort a list
